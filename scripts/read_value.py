8f271c33d8854a38b0df08395d4739cba8a5d47b
ArnaudTheBuilder <40626382+ArnoTower@users.noreply.github.com>
2022-05-20 21:32:55 -0400

    Update

--- scripts/read_value.py
+++ scripts/read_value.py
@@ -2,7 +2,11 @@ from brownie import SimpleStorage, accounts, config
 
 
 def read_contract():
-    pass
+    simple_storage = SimpleStorage[-1]
+    # go take the index thats one less than the length
+    # ABI whenever interacting with smart contract (brownie already knows this)
+    # Address (Brownie already knows your contract address)
+    print(simple_storage.retrieve())
 
 
 def main():
